0165fe27945e1667848e78a4b379c7bbc6959dc8
Gyujin Cho <gyujincho@lunit.io>
2019-07-24 20:51:51 +0900

    Implement Children Reconciliation for dom reuse

--- src/didact/renderWithReconciler.ts
+++ src/didact/renderWithReconciler.ts
@@ -68,18 +68,46 @@ function instantiate(element: DidactElement): DidactInstance {
   }
 }
 
+function reconcileChildren(instance, element) {
+  const dom = instance.dom
+  const childInstances = instance.childInstances
+  const nextChildElements = element.props.children || []
+  const newChildInstances = []
+
+  const count = Math.max(childInstances.length, nextChildElements.length)
+
+  for (let i = 0; i < count; i++) {
+    const childInstance = childInstances[i]
+    const childElement = nextChildElements[i]
+    const newChildInstance = reconcile(dom, childInstance, childElement)
+    newChildInstances.push(newChildInstance)
+  }
+
+  return newChildInstances
+}
+
 function reconcile(
   parentDom: HTMLElement,
   instance: DidactInstance,
   element: DidactElement
 ) {
-  const newInstance = instantiate(element)
   if (instance === null) {
+    /** Create instance */
+    const newInstance = instantiate(element)
     parentDom.appendChild(newInstance.dom)
+    return newInstance
+  } else if (instance.element.type === element.type) {
+    /** Update instance */
+    updateDomProperties(instance.dom, instance.element.props, element.props)
+    instance.childInstances = reconcileChildren(instance, element)
+    instance.element = element
+    return instance
   } else {
+    /** Replace instance */
+    const newInstance = instantiate(element)
     parentDom.replaceChild(newInstance.dom, instance.dom);
+    return newInstance
   }
-  return newInstance;
 }
 
 export function render(element, container) {
